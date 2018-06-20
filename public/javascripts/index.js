307d0c774f8dfe101f9170b97b6b5bc072f666de
yklim <yk@mobileads.com>
2018-06-20 18:19:37 +0800

    Message twitter only if user log in with twitter

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -75,11 +75,14 @@ var app = {
 					if (response.data.couponLink) {
 						this.initResult('win', response.data.couponLink);
 						var message = '綾鷹クーポンが当たりました！ ' + response.data.couponLink;
-						user.messageTwitter(message);
+						
 
 						if (user.info.id.indexOf('@') > -1) { // login via email
 		        	var emailContent = '<head><meta charset="utf-8"></head><div style="text-align:center;font-weight:600;color:#FF4244;font-size:28px;">おめでとうございます</div><br><br><div style="text-align:center;font-weight:600;">クーポンが当たりました！</div><a href="' + response.data.couponLink + '" target="_blank" style="text-decoration:none;"><button style="display:block;margin:20px auto;margin-bottom:40px;border-radius:5px;background-color:#E54C3C;border:none;color:white;width:200px;height:50px;font-weight:600;">クーポンを受取る</button></a>';
 	        	  user.sendEmail(user.info.id, 'FamilyMart Coupon Link', emailContent);
+						}
+						else {
+							user.messageTwitter(message);
 						}
 						// user.passResult(user.info.id, flag, user.source, response.data.couponLink);
 					}
